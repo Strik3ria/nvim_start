869128be62706b79b148835da07c2b5576dd1dd6
Robert Coones <Strik3ria>
2023-11-13 06:20:27 -0600

    Updated config

--- init.lua
+++ init.lua
@@ -209,9 +209,9 @@ require('lazy').setup({
       -- requirements installed.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable 'cmake' == 1
         end
       }
     }
@@ -512,6 +512,7 @@ local servers = {
   -- gopls = {},
   pyright = {},
   puppet = {},
+  terraformls = {},
   -- rust_analyzer = {},
   -- tsserver = {},
 
